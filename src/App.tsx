ce215910d345068fe676453782bb35a26458612b
chandu3292 <karrichandu03@gmail.com>
2026-05-21 09:41:49 +0530

    Add Vercel Analytics + Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -37,6 +39,8 @@ export default function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
+      <Analytics />
+      <SpeedInsights />
     </>
   )
 }
